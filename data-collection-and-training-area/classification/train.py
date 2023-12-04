1fd768f03419c88de008183061d5b955e4f6ccaf
Sampad Sikder <60497178+SampadSikder@users.noreply.github.com>
2023-12-04 16:26:47 +0600

    Made the classification model more complex and now it performs better

--- data-collection-and-training-area/classification/train.py
+++ data-collection-and-training-area/classification/train.py
@@ -14,23 +14,28 @@ Y_onehot = encoder.fit_transform(Y.reshape(-1, 1))
 # Step 2: Split the Data
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y_onehot, test_size=0.2, random_state=42)
 
-# Step 4: Model Architecture
+# Step 4: Improved Model Architecture
 model = models.Sequential()
 model.add(layers.Conv1D(32, kernel_size=3, activation='relu', input_shape=(16384, 1)))
 model.add(layers.MaxPooling1D(pool_size=2))
+model.add(layers.Conv1D(64, kernel_size=3, activation='relu'))
+model.add(layers.MaxPooling1D(pool_size=2))
+model.add(layers.Conv1D(128, kernel_size=3, activation='relu'))
+model.add(layers.MaxPooling1D(pool_size=2))
 model.add(layers.Flatten())
 model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(21, activation='softmax'))
 
-# Step 5: Compile the Model
+# Step 5: Compile the Improved Model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Step 6: Training
-model.fit(X_train, Y_train, epochs=10, batch_size=32, validation_split=0.1)
+model.fit(X_train, Y_train, epochs=20, batch_size=32, validation_split=0.1)
 
-# Step 7: Evaluate the Model
+# Step 7: Evaluate the Improved Model
 test_loss, test_acc = model.evaluate(X_test, Y_test)
 print(f'Test accuracy: {test_acc}')
 
-# Step 8: Save the Model
-model.save('video_classification_model.h5')
+# Step 8: Save the Improved Model
+model.save('improved_video_classification_model.h5')

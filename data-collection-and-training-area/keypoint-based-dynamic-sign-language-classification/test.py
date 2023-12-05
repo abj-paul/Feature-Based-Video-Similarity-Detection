4ab78d0fe18e7599b34842c04afea91afd88adb9
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-12-05 18:44:17 +0600

    test code working fine but model is still trash

--- data-collection-and-training-area/keypoint-based-dynamic-sign-language-classification/test.py
+++ data-collection-and-training-area/keypoint-based-dynamic-sign-language-classification/test.py
@@ -8,14 +8,14 @@ from data_collection.collect_data_from_camera import get_keypoints
 import mediapipe as mp
 
 # Load the trained model
-labels = joblib.load("labels.joblib")
+labels = joblib.load("Y.joblib")
 model = load_model('video_classification_model.h5')
 
 # Function to get keypoints and preprocess the frame
-def preprocess_frame(frame, holistic_model):
-    resized_frame = cv2.resize(frame, (1662, 16))
-    keypoint_frame, keypoints = get_keypoints(resized_frame, holistic_model)
-    return keypoint_frame, keypoints
+def preprocess_frame(frame, holistic_model, prev_keypoints):
+    keypoint_frame, keypoints = get_keypoints(frame, holistic_model)
+    prev_keypoints.append(keypoints)
+    return keypoint_frame, prev_keypoints
 
 # Open the camera
 cap = cv2.VideoCapture("http://192.168.0.101:4747/video")  # Use 0 for the default camera
@@ -24,6 +24,7 @@ cap = cv2.VideoCapture("http://192.168.0.101:4747/video")  # Use 0 for the defau
 frame_skip_interval = 5  # Skip every 5 frames
 
 frame_count = 0
+keypoints_sequence = []
 
 # Holistic model for keypoints
 mp_holistic = mp.solutions.holistic
@@ -40,21 +41,28 @@ while True:
     if frame_count % frame_skip_interval != 0:
         continue
 
-    # Preprocess the frame and get keypoints
-    keypoint_frame, keypoints = preprocess_frame(frame, holistic_model)
+    # Preprocess the frame and accumulate keypoints
+    keypoint_frame, keypoints_sequence = preprocess_frame(frame, holistic_model, keypoints_sequence)
 
-    # Make a prediction
-    predictions = model.predict(keypoints)
-    print(predictions)
+    # Make a prediction when enough frames are accumulated
+    if len(keypoints_sequence) == 16:
+        keypoint_frame = np.array(keypoint_frame)
+        predictions = model.predict(np.expand_dims(np.expand_dims(keypoints_sequence, axis=0), axis=-1))
+        print(predictions)
 
-    # Get the predicted class
-    predicted_class = np.argmax(predictions)
+        # Get the predicted class
+        predicted_class = np.argmax(predictions)
 
-    # Get the corresponding label
-    predicted_label = labels[predicted_class]
+        # Get the corresponding label
+        predicted_label = labels[predicted_class]
 
-    # Display the frame and predicted label
-    cv2.putText(keypoint_frame, f"Predicted Label: {predicted_label}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        # Display the frame and predicted label
+        cv2.putText(keypoint_frame, f"Predicted Label: {predicted_label}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        print(f"{predicted_label}")
+        keypoints_sequence = []
+    else:
+        cv2.putText(keypoint_frame, f"Accumulated Frames: {len(keypoints_sequence)}/20", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
     cv2.imshow('Video Classification', keypoint_frame)
 
     # Break the loop if 'q' is pressed
